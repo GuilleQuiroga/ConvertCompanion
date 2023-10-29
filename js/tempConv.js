79c2b22e4934ae03010d54bed68d0b3038b37c98
mamakapu <guillermoquirogagallego@gmail.com>
2023-10-29 17:56:25 +0100

    Implementamos lógica de la conversión de temps

---
+++ js/tempConv.js
@@ -0,0 +1,127 @@
+// Botones
+const btnTmp = document.querySelector("#btnTmp");
+
+// Temperaturas
+const celsius = document.querySelector("#celsius");
+const fahren = document.querySelector("#fahren");
+const kelvin = document.querySelector("#kelvin");
+
+const temps = document.getElementsByClassName(".temps");
+
+/**
+ * Conversión de celsius a fahrenheit y a kelvin
+ * @returns fahrenheit
+ */
+const convCelsiusFahren = () => {
+	let grCelsiusFahren = document.querySelector("#celsius").value;
+	let fahren = (grCelsiusFahren * 9) / 5 + 32; // ( °C × 9 / 5) + 32 =  °F
+	return fahren;
+};
+
+const convCelsiusKelvin = () => {
+	let grCelsiusKelvin = document.querySelector("#celsius").value;
+	let celsius;
+
+	if (grCelsiusKelvin == "") {
+		celsius = 0;
+		let kelvin = celsius + 273.15;
+		return kelvin;
+	} else {
+		celsius = parseFloat(grCelsiusKelvin);
+		let kelvin = celsius + 273.15;
+		return kelvin;
+	}
+};
+
+/**
+ * Conversión de fahrenheit a celsius y a kelvin
+ * @returns fahrenheit
+ */
+const convFahrenCelsius = () => {
+	let grFahrenCelsius = document.querySelector("#fahren").value;
+
+	if (grFahrenCelsius == "") {
+		grFahrenCelsius = 0;
+	} else {
+		grFahrenCelsius = parseFloat(grFahrenCelsius);
+	}
+	let celsius = (grFahrenCelsius - 32) * 5;
+	return (celsius / 9).toFixed(2);
+};
+
+const convFahrenKelvin = () => {
+	let grFahrenKelvin = document.querySelector("#fahren").value;
+
+	if (grFahrenKelvin == "") {
+		grFahrenKelvin = 0;
+	} else {
+		grFahrenKelvin = parseFloat(grFahrenKelvin);
+	}
+	let kelvin = (grFahrenKelvin - 32) * 5;
+	return (kelvin / 9 + 273.15).toFixed(2);
+};
+
+/**
+ * Conversión de kelvin a celsius y a fahrenheit
+ * @returns kelvin
+ */
+const convKelvinCelsius = () => {
+	let grKelvinCelsius = document.querySelector("#kelvin").value;
+
+	if (grKelvinCelsius == "") {
+		grKelvinCelsius = 0;
+	} else {
+		grKelvinCelsius = parseFloat(grKelvinCelsius);
+	}
+	let celsius = grKelvinCelsius - 273.15;
+	return celsius.toFixed(2);
+};
+
+const convKelvinFahren = () => {
+	let grKelvinFahren = document.querySelector("#kelvin").value;
+
+	if (grKelvinFahren == "") {
+		grKelvinFahren = 0;
+	} else {
+		grKelvinFahren = parseFloat(grKelvinFahren);
+	}
+	let fahren = ((grKelvinFahren - 273.15) * 9) / 5 + 32;
+	// return (fahren / 5 + 32).toFixed(2);
+	return fahren.toFixed(2);
+};
+
+/**
+ * Llamada funciones celsius
+ */
+
+celsius.addEventListener("keyup", function () {
+	let resCelsiusFahren = convCelsiusFahren();
+	fahren.value = `${resCelsiusFahren} ºF`;
+
+	let resCelsiusKelvin = convCelsiusKelvin();
+	kelvin.value = `${resCelsiusKelvin} K`;
+});
+
+/**
+ * Llamada funciones Fahrenheit
+ */
+
+fahren.addEventListener("keyup", function () {
+	let resFahrenCelsius = convFahrenCelsius();
+	celsius.value = `${resFahrenCelsius} ºF`;
+
+	let resFahrenKelvin = convFahrenKelvin();
+	kelvin.value = `${resFahrenKelvin} ºK`;
+});
+
+/**
+ * Llamada funciones Kelvin
+ */
+
+kelvin.addEventListener("keyup", function () {
+	let resKelvinCelsius = convKelvinCelsius();
+	celsius.value = `${resKelvinCelsius} ºC`;
+
+	let resKelvinFahren = convKelvinFahren();
+	fahren.value = `${resKelvinFahren} ºF`;
+});
